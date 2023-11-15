092e6b0ea6a10aeb27c1189fa53ff249367c0c28
benjamin6345 <132579766+benjamin6345@users.noreply.github.com>
2023-11-15 22:20:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,14 +5,14 @@ from collections import defaultdict
 
 st.title("HeartGuardian")
 col1, col2 = st.columns(2)
+
 with col1:
     st.subheader("Basic Information基本資料")
-
+    age = st.slider("Age年齡", 5, 100, 1)
     weight = st.slider("Weight體重(Kg)", 35, 200, 35)
     height = st.slider("Height高度(m)", 0.80, 2.4, 0.80)
-    age = st.slider("Age年齡", 5, 100, 1)
+    smoker = st.radio('Smoker吸煙者', ["Yes是", "No否"])
     
-    smoke = st.radio("Smoke(d)(曾)吸煙", ["Yes是", "No否"])
 
     
     
@@ -21,31 +21,32 @@ with col2:
     st.subheader("Health History健康歷史")
     stroke = st.radio("Have/Had Stroke(曾)中風", ["Yes是", "No否"])
     diabetic = st.radio("Had/Has Diabetes(曾)有糖尿病", ["Yes是", "Yes(During Pregnancy)是（當懷孕)","No否"])
+    diffwalk = st.radio("Difficulty Walking走路困難", ["Yes是", "No否"])
+    highbp = st.radio('High Blood Pressure 高血壓', ["Yes是", "No否"])
+    
 
 
-with open("model.pkl", 'rb') as file:
+with open("model2.pkl", 'rb') as file:
     model = pickle.load(file)
 
 def submit_actions():
+    age_final = age/65
     BMI = weight / (height * height)
-    BMI = BMI / 35
-    smoke_final = 1 if smoke == 'Yes是' else 0
-
-    age_final = age/80
-    diabetic_final = 1 if diabetic == 'Yes是' else 2 if diabetic == "Yes(During Pregnancy)是（當懷孕)" else 0
+    BMI = BMI / 40
+    smoker_final = 1 if smoker == 'Yes是' else 0
 
     stroke_final = 1 if stroke == 'Yes是' else 0
+    diabetic_final = 1 if diabetic == 'Yes是' else 2 if diabetic == "Yes(During Pregnancy)是（當懷孕)" else 0
+    diffwalk_final = 1 if diffwalk == 'Yes是' else 0
+    highbp_final = 1 if highbp == 'Yes是' else 0
     
-    result = [BMI, smoke_final, age_final, diabetic_final, stroke_final]
+    
+    result = [age_final, diffwalk_final, highbp_final, stroke_final, diabetic_final, smoker_final, BMI]
 
     arr = np.array(result).reshape(1, -1)
     y_pred = np.round(model.predict(arr))[0]
    
 
-
-    
-    
-    
     
     if y_pred == 1:
         st.subheader('Your body condition can be vulnerable to heart disease.')
@@ -56,8 +57,11 @@ def submit_actions():
             st.markdown('Please manage your body sugar level. 請控制身體血糖量。 \n')
         if BMI > 23:
             st.markdown('Please control your weight/BMI. 請控制你的體重/BMI。\n')
-        if smoke_final == 1:
+        if smoker_final == 1:
             st.write('Please stop smoking. 請不要吸煙。')
+            
+        if highbp_final == 1:
+            st.write('Please maintain a good lifestyle to maintain a lower blood pressure. 請保持健康的身活習慣以減低血壓。')
             
     else:
         st.subheader('Your body condition are not vulnerable to heart disease.')
