c3c0cc2f88635cf2eb85dc7c46c339d9cc99cdc7
benjamin6345 <132579766+benjamin6345@users.noreply.github.com>
2023-11-19 09:58:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,15 @@ import pickle
 import numpy as np
 from collections import defaultdict
 
+#Sex, highchol, physAct, veggies, fruits, num_berer
+
 st.title("HeartGuardian")
 col1, col2 = st.columns(2)
 
 with col1:
     st.subheader("Basic Information基本資料")
     age = st.slider("Age年齡", 5, 100, 1)
+    sex = st.radio('Sex性別', ['Male男', 'Female女'])
     weight = st.slider("Weight體重(Kg)", 35, 200, 35)
     height = st.slider("Height高度(m)", 0.80, 2.4, 0.80)
     smoker = st.radio('Smoker吸煙者', ["Yes是", "No否"])
@@ -23,10 +26,19 @@ with col2:
     diabetic = st.radio("Had/Has Diabetes(曾)有糖尿病", ["Yes是", "Yes(During Pregnancy)是（當懷孕)","No否"])
     diffwalk = st.radio("Difficulty Walking走路困難", ["Yes是", "No否"])
     highbp = st.radio('High Blood Pressure 高血壓', ["Yes是", "No否"])
+    highchol = st.radio('High Cholesterol 高膽固醇', ["Yes是", "No否"])
+    physcAct = st.radio('Sport in the last 30 days過去三十天有做運動', ["Yes是", "No否"])
+    veggies = st.radio('Consume vegetables every day每天吃菜', ["Yes是", "No否"])
+    fruits = st.radio('Consume Fruits every day每天吃水果', ["Yes是", "No否"])
+    num_beer = st.slider('Cups of alcohol per week每週飲酒量(杯)', 1, 20, 1)
+    bad_menthealth_days = st.slider('Number of days suffering for mental issues in the past 30 days過去三十天有多少天是心理狀況不佳的', 1, 30, 1)
+    bad_physhealth_days = st.slider('Number of days suffering for physics issues in the past 30 days過去三十天有多少天是身體狀況不佳的', 1, 30, 1)
+    
+    
     
 
 
-with open("model3.pkl", 'rb') as file:
+with open("heartmodel.pkl", 'rb') as file:
     model = pickle.load(file)
 
 def submit_actions():
@@ -34,14 +46,30 @@ def submit_actions():
     BMI = weight / (height * height)
     BMI = BMI / 40
     smoker_final = 1 if smoker == 'Yes是' else 0
+    sex_final = 1 if sex == 'Male男' else 0
+    
 
     stroke_final = 1 if stroke == 'Yes是' else 0
     diabetic_final = 1 if diabetic == 'Yes是' else 2 if diabetic == "Yes(During Pregnancy)是（當懷孕)" else 0
     diffwalk_final = 1 if diffwalk == 'Yes是' else 0
     highbp_final = 1 if highbp == 'Yes是' else 0
+    highchol_final = 1 if highchol == 'Yes是' else 0
+    physAct_final = 1 if physcAct == 'Yes是' else 0
+    veggies_final = 1 if veggies == 'Yes是' else 0
+    fruits_final = 1 if fruits == 'Yes是' else 0
+    bad_menthealth_days_final = bad_menthealth_days/30
+    bad_physhealth_days_final = bad_physhealth_days/30
+    
+    if sex == 1: #Male
+        highalc_final = 1 if num_beer >= 14 else 0
+    
+    else:
+        highalc_final = 1 if num_beer >= 7 else 0
+        
     
     
-    result = [age_final, diffwalk_final, highbp_final, stroke_final, diabetic_final, smoker_final, BMI]
+    
+    result = [highbp_final, highchol_final, BMI, smoker_final, stroke_final, diabetic_final, physAct_final, fruits_final, veggies_final, highalc_final, bad_menthealth_days_final, bad_physhealth_days_final, diffwalk_final, sex_final, age_final]
 
     arr = np.array(result).reshape(1, -1)
     y_pred = np.round(model.predict(arr))[0]
@@ -52,21 +80,19 @@ def submit_actions():
         st.subheader('Your body condition can be vulnerable to heart disease.')
         st.subheader('你較容易患有心臟病。')
         
-        st.markdown('Professional Body Checks in Hospitals are Recommended. \n建議到醫院進行專業的身體檢查\n')
-        # if diabetic_final == 1:
-        #     st.markdown('Please manage your body sugar level. 請控制身體血糖量。 \n')
-        # if BMI > 23:
-        #     st.markdown('Please control your weight/BMI. 請控制你的體重/BMI。\n')
-        # if smoker_final == 1:
-        #     st.write('Please stop smoking. 請不要吸煙。')
+        if diabetic_final == 1:
+            st.markdown('Please manage your body sugar level. 請控制身體血糖量。 \n')
+        if BMI > 23:
+            st.markdown('Please control your weight/BMI. 請控制你的體重/BMI。\n')
+        if smoker_final == 1:
+            st.write('Please stop smoking. 請不要吸煙。')
             
-        # if highbp_final == 1:
-        #     st.write('Please maintain a good lifestyle to maintain a lower blood pressure. 請保持健康的身活習慣以減低血壓。')
+        if highbp_final == 1:
+            st.write('Please maintain a good lifestyle to maintain a lower blood pressure. 請保持健康的身活習慣以減低血壓。')
             
     else:
         st.subheader('Your body condition are not vulnerable to heart disease.')
         st.subheader('你較不容易患上心臟病。')
-        st.markdown('Professional Body Checks in Hospitals are not Necessary. \n不必到醫院進行專業的身體檢查\n')
     
     
 button = st.button("Submit提交", on_click=submit_actions)
